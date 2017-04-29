fdc2431a1a79442e328ab46cf11049638b63e86a
Johnny Perez <johnny.perez2@northwestern.edu>
2017-04-29 11:03:20 -0500

    none

---
+++ rsvpserver.js
@@ -0,0 +1,76 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Star Wars tables (DATA)
+// =============================================================
+var tables = [{
+  username: "",
+  name: "",
+  phoneNumber: "",
+  email: ""
+}];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/reservation", function(req, res) {
+  res.sendFile(path.join(__dirname, "reservation.html"));
+});
+
+// Search for Specific Character (or all tables) - provides JSON
+app.get("/api/:tables?", function(req, res) {
+  var chosen = req.params.tables;
+
+  if (chosen) {
+    console.log(chosen);
+
+    for (var i = 0; i < tables.length; i++) {
+      if (chosen === tables[i].routeName) {
+       return res.json(tables[i]);
+      }
+    }
+    return res.json(false);
+  }
+  return res.json(tables);
+});
+
+// Create New tables - takes in JSON input
+app.post("/api/new", function(req, res) {
+  var newcharacter = req.body;
+  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newcharacter);
+
+  tables.push(newcharacter);
+
+  res.json(newcharacter);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
